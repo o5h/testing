3f958648dbe4acfa3ee256d39ed50bec902d2b0f
Oleksandr Shaposhnikov <olexander.shaposhnikov@gmail.com>
2019-02-25 23:06:21 +0100

    assert

---
+++ assert/assert.go
@@ -0,0 +1,118 @@
+package assert
+
+import (
+	"math"
+	"reflect"
+	"testing"
+)
+
+func True(t *testing.T, actual interface{}) {
+	t.Helper()
+	Eq(t, actual, true)
+}
+
+func False(t *testing.T, actual interface{}) {
+	t.Helper()
+	Eq(t, actual, false)
+}
+
+func Nil(t *testing.T, actual interface{}) {
+	t.Helper()
+	if actual != nil {
+		t.Fatalf("Expected nil, was %v", actual)
+	}
+}
+
+func Eq(t *testing.T, actual interface{}, expected interface{}) {
+	t.Helper()
+	actualType := reflect.TypeOf(actual)
+	expectedType := reflect.TypeOf(expected)
+	if actualType != expectedType {
+		t.Fatalf("Expected %T, was %T", expectedType, actualType)
+	}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("Expected %v, was %v", expected, actual)
+	}
+}
+
+func NotEq(t *testing.T, actual interface{}, expected interface{}) {
+	t.Helper()
+	actualType := reflect.TypeOf(actual)
+	expectedType := reflect.TypeOf(expected)
+	if actualType != expectedType {
+		t.Fatalf("Expected %T, was %T", expectedType, actualType)
+	}
+	if reflect.DeepEqual(actual, expected) {
+		t.Fatalf("Expected not %v, was %v", expected, actual)
+	}
+}
+
+func EqEpsilon(t *testing.T, actual interface{}, expected interface{}, epsilon float64) {
+	t.Helper()
+	act := asFloat64(t, actual)
+	exp := asFloat64(t, expected)
+	if math.Abs(act-exp) > epsilon {
+		t.Fatalf("Expected %f, was %f (+-%f)", exp, act, epsilon)
+	}
+}
+
+func EqSlice(t *testing.T, actual interface{}, expected interface{}, epsilon float64) {
+	t.Helper()
+	var act reflect.Value
+	var exp reflect.Value
+
+	switch reflect.TypeOf(actual).Kind() {
+	case reflect.Slice:
+		act = reflect.ValueOf(actual)
+	default:
+		t.Fatalf("Slice is expected, was %T", actual)
+	}
+
+	switch reflect.TypeOf(expected).Kind() {
+	case reflect.Slice:
+		exp = reflect.ValueOf(expected)
+	default:
+		t.Fatalf("Slice is expected, was %T", expected)
+	}
+
+	if exp.Len() != act.Len() {
+		t.Fatalf("Slice len expected %v, was %v", exp.Len(), act.Len())
+	}
+
+	for i := 0; i < exp.Len(); i++ {
+		expValue := exp.Index(i).Interface()
+		actValue := act.Index(i).Interface()
+		EqEpsilon(t, actValue, expValue, epsilon)
+	}
+}
+
+func asFloat64(t *testing.T, i interface{}) float64 {
+	switch v := i.(type) {
+	case uint:
+		return float64(v)
+	case int:
+		return float64(v)
+	case int8:
+		return float64(v)
+	case uint8:
+		return float64(v)
+	case int16:
+		return float64(v)
+	case uint16:
+		return float64(v)
+	case int32:
+		return float64(v)
+	case uint32:
+		return float64(v)
+	case int64:
+		return float64(v)
+	case uint64:
+		return float64(v)
+	case float32:
+		return float64(v)
+	case float64:
+		return v
+	}
+	t.Fatalf("Unsupported type %T for %v", i, i)
+	return 0
+}
